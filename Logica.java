51d304bb248bf9ddafaeee02cdfec79e7bc75721
Juan Narvaez <camilolovegood@gmail.com>
2015-04-10 22:58:02 -0500

    Correccion logica

--- Logica.java
+++ Logica.java
@@ -15,19 +15,22 @@ public class Logica {
 		
         Vector<String> resultado = new Vector<String>();
         int tam = puntos.size();
-        double calculoFormula;
+        double calculoUno, calculoDos;
              
 	for(int i = 0 ; i < tam ; i++){
                       
-            calculoFormula = Math.pow((puntos.get(i).get(0) - cA1), 2) +
+            calculoUno = Math.pow((puntos.get(i).get(0) - cA1), 2) +
                              Math.pow((puntos.get(i).get(1) - cA2), 2);
-            if(calculoFormula <= rA && calculoFormula <= rB){
+            calculoDos  = Math.pow((puntos.get(i).get(0) - cB1), 2) +
+                             Math.pow((puntos.get(i).get(1) - cB2), 2);
+            
+            if(calculoUno <= Math.pow(rA,2) && calculoDos <= Math.pow(rB,2)){
                 resultado.add("Dentro de ambas circunferencias");
             }else{
-		if(calculoFormula <= Math.pow(rA,2)){
+		if(calculoUno <= Math.pow(rA,2)){
 			resultado.add("Dentro de la circunferencia 1");
 		}else{
-			if( calculoFormula <= Math.pow(rB,2)){
+			if( calculoDos <= Math.pow(rB,2)){
 				resultado.add("Dentro de la circunferencia 2");			
 			}else{
 				resultado.add("Por fuera de ambas circunferencias");
